80e49c8532cfa20b055f3dc804c1e772c0ec8038
tohbay <tobechukwuobitube@gmail.com>
2024-07-07 19:04:31 +0100

    add user location and eas build configuration for android

--- App.tsx
+++ App.tsx
@@ -1,22 +1,38 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import StepsMetric from "./src/components/StepsMetric";
 import RingProgress from "./src/components/RingProgress";
 import { useEffect, useState } from "react";
-import appleHealthKit, {
-  HealthInputOptions,
-  HealthKitPermissions,
-  HealthUnit,
-} from "react-native-health";
 import useHealthData from "./src/hooks/useHealthData";
+import * as Location from "expo-location";
 
 const STEP_GOAL = 10000;
 
 export default function App() {
+  const [userLocation, setUserLocation] = useState<
+    Location.LocationObject | undefined
+  >();
   const { steps, flights, distance } = useHealthData(new Date("2024-07-03"));
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status == "granted") {
+        console.log("Permission granted successfully");
+      } else {
+        console.log("Permission denied");
+      }
+
+      const location = await Location.getCurrentPositionAsync();
+      setUserLocation(location);
+    })();
+  }, []);
 
   return (
     <View style={styles.container}>
+      <Text style={{ marginTop: 20, marginBottom: 20 }}>
+        {JSON.stringify(location)}
+      </Text>
       <RingProgress
         radius={150}
         strokeWidth={50}
